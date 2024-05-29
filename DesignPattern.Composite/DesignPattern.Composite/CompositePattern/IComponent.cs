70a2cc0e3ddd297c50a7dcafc1d3ba5e0daea6e3
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-05-29 16:53:00 +0300

    productComposite

---
+++ DesignPattern.Composite/DesignPattern.Composite/CompositePattern/IComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern.Composite.CompositePattern
+{
+    public interface IComponent
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        int TotalCount();
+        string Display();
+    }
+}
---
+++ DesignPattern.Composite/DesignPattern.Composite/CompositePattern/ProductComponent.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignPattern.Composite.CompositePattern
+{
+    public class ProductComponent : IComponent
+    {
+        public ProductComponent(int id, int name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get ; set  ; }
+        public int Name { get  ; set ; }
+
+        public string Display()
+        {
+            return $"<li class='list-group-item'>{Name}</li>";
+        }
+
+        public int TotalCount()
+        {
+            return 1;
+        }
+    }
+}
