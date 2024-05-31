8c4a9af6c83eaa7363961a760a0c6f1243d1d85e
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-05-31 16:17:45 +0300

    Facade design pattern new

---
+++ DesignPattern.Facade/DesignPattern.Facade/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Facade.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=SERCAN; initial catalog=FacadeDP; integrated security=true");
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ DesignPattern.Facade/DesignPattern.Facade/DAL/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPattern.Facade.DAL
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerCity { get; set; }
+    }
+}
