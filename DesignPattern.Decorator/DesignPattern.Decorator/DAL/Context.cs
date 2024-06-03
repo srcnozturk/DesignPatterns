76946aa2d9c2befb3b5871ac03b8913cffd6925a
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-06-03 15:33:54 +0300

    addNewDecoratorDesignPattern

---
+++ DesignPattern.Decorator/DesignPattern.Decorator/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Decorator.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=SERCAN; initial catalog=DecoratorDPDb; integrated security=true");
+        }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
