545c2aa40bc672483e025e0ebc469a49cf449aab
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-06-03 09:51:43 +0300

    ProductStock,OrderFacade,AddOrderDetail

---
+++ DesignPattern.Facade/DesignPattern.Facade/FacadePattern/OrderFacade.cs
@@ -0,0 +1,32 @@
+﻿using DesignPattern.Facade.DAL;
+
+namespace DesignPattern.Facade.FacadePattern
+{
+    public class OrderFacade
+    {
+        Order order = new Order();
+        OrderDetail orderDetail = new OrderDetail();
+        ProductStock productStock = new ProductStock();
+
+        AddOrder addOrder = new AddOrder();
+        AddOrderDetail addOrderDetail = new AddOrderDetail();
+
+        public void CompleteOrder(int customerID,int productID,int orderID,int productCount,
+            decimal productPrice)
+        {
+            order.CustomerID = customerID;
+            addOrder.AddNewOrder(order);
+
+            orderDetail.OrderID = orderID; 
+            orderDetail.CustomerID = customerID;
+            orderDetail.ProductID = productID;
+            orderDetail.ProductCount = productCount;
+            orderDetail.ProductPrice = productPrice;
+            decimal totalProductPrice=productCount * productPrice;
+            orderDetail.ProductTotalPrice = totalProductPrice;
+            addOrderDetail.AddNewOrderDetail(orderDetail);
+
+            productStock.StockDecrease(productID, productCount);
+        }
+    }
+}
