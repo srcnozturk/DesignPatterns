3ae2d4fdc4f9586681a9031167a49f033dce62b9
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-06-04 10:37:07 +0300

    designPatternDecorator docs and encrypt subject and content

---
+++ DesignPattern.Decorator/DesignPattern.Decorator/DecoratorPattern2/EncrypttoBySubjectDecorator.cs
@@ -0,0 +1,38 @@
+﻿using DesignPattern.Decorator.DAL;
+using System;
+
+namespace DesignPattern.Decorator.DecoratorPattern2
+{
+    public class EncrypttoBySubjectDecorator : Decorator
+    {
+        private readonly ISendMessage _sendMessage;
+        Context context=new Context();
+        public EncrypttoBySubjectDecorator(ISendMessage sendMessage) : base(sendMessage)
+        {
+            _sendMessage = sendMessage;
+        }
+
+        public void SendMessageEncryptoSubject(Message message)
+        {
+            message.MessageSender   = "İnsan Kaynakları";
+            message.MessageReceiver = "Yazılım Ekibi";
+            message.MessageContent  = "Saat 12:00'de Toplantı var";
+            message.MessageSubject  = "Toplantı";
+            string data             = "";
+            data = message.MessageSubject;
+            char[] chars = data.ToCharArray();
+            foreach (var item in chars)
+            {
+                message.MessageSubject += Convert.ToChar(item + 3).ToString();
+            }
+            context.Messages.Add(message);
+            context.SaveChanges();
+        }
+
+        public override void SendMessage(Message message)
+        {
+            base.SendMessage(message);
+            SendMessageEncryptoSubject(message);
+        }
+    }
+}
